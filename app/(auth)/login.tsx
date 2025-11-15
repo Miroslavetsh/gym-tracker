c90f19d531d9957f12b5513df17052801d15a018
Myroslav Toloshnyi <dfgshte@gmail.com>
2025-11-15 09:08:56 +0200

    NOISSUE Remove api diagnostics import

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,19 +1,14 @@
 import { useRouter } from "expo-router";
-import React, { useEffect } from "react";
+import React from "react";
 import { StyleSheet, View } from "react-native";
 
 import { AuthContainer } from "@/components/auth/auth-container";
 import { AuthFooter } from "@/components/auth/auth-footer";
 import { AuthHeader } from "@/components/auth/auth-header";
 import { LoginForm } from "@/components/auth/login-form";
-import { logApiInfo } from "@/utils/apiDiagnostics";
 
 export default function LoginScreen() {
   const router = useRouter();
-
-  useEffect(() => {
-    if (__DEV__) logApiInfo();
-  }, []);
 
   return (
     <AuthContainer>
